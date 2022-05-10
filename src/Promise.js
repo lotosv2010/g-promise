b3c4d79765849b0e079e1e820d50770aca81e42e
gaowenbin <lotosv2010@163.com>
2022-05-10 14:49:10 +0800

    feat(promise): then 方法链式调用实现

--- src/Promise.js
+++ src/Promise.js
@@ -3,6 +3,54 @@ const PENDING = 'PENDING'     // 等待状态
 const FULFILLED = 'FULFILLED' // 成功状态
 const REJECTED = 'REJECTED'   // 失败状态
 
+const resolvePromise = (promise2, x, resolve, reject) => {
+  // 判断可能你的 promise 要和别人的 promise 来混用
+  // 可能不同的promise库之间要相互调用
+
+  // 为了考虑别人的 promise 不健壮所以我们需要自己取判断一下，
+  // 如果调用失败不能再调成功，如果调用成功不能再调失败
+  // 不能多次调用成功或失败
+  let called;
+
+  // 1.x 如果和 promise 使用一个对象，x永远不能成功或失败，所以就死循环了，我们要直接抛出一个错误
+  if(promise2 == x) {
+    return reject(new TypeError('Chaining cycle detected for promise'));
+  }
+
+  // todo 判断 x 的状态
+  // 2.判断 x 是不是promise
+  if((typeof x === 'object' && x!== null) || typeof x === 'function') {
+    // x 是一个对象或这函数
+    try {
+      let then = x.then; // 取出 then 方法，这个 then 方法采用 defineProperty 定义的
+      if(typeof then === 'function') {
+        // 判断 then 是不是一个函数，如果 then 不是一个函数，说明 x 不是 promise
+        // todo：注意：x.then(() => {}, () => {}) 这样调用会再次取 then
+        then.call(x, y => { // 如果 x 是一个 promise 就采用这个 promise 的返回结果
+          if(called) return;
+          called = true;
+          // 如果 y 是一个 promise 继续解析成功的值
+          resolvePromise(promise2, y, resolve, reject);
+        }, r => {
+          if(called) return;
+          called = true;
+          reject(r);
+        })
+      } else {
+        // x 是一个对象，例如：x = {then:123}
+        resolve(x);
+      }
+    } catch (e) {
+      if(called) return;
+      called = true;
+      reject(e); // 去 then 失败了，直接触发 promise2 的失败逻辑
+    }
+  } else {
+    // x 肯定不是 promise
+    resolve(x);
+  }
+}
+
 class Promise {
   status = PENDING; // promise 状态
   value = undefined; // 成功的数据
@@ -26,21 +74,40 @@ class Promise {
     executor(resolve, reject);
   }
   then(onFulFilled, onRejected) {
-    // 判断状态
-    if(this.status === FULFILLED) {
-      onFulFilled(this.value);
-    } else if(this.status === REJECTED) {
-      onRejected(this.reason);
-    } else {
-      // 订阅的过程
-      // 将成功和失败回调存储起来
-      this.onResolveCallback.push(() => {
-        onFulFilled(this.value);
-      });
-      this.onRejectCallback.push(() => {
-        onRejected(this.reason)
-      });
-    }
+    const promise2 = new Promise((resolve, reject) => {
+      // 判断状态
+      if(this.status === FULFILLED) {
+        setTimeout(() => { // TODO: 此处用异步逻辑为了解决 Uncaught ReferenceError: Cannot access 'promise2' before initialization
+          const x = onFulFilled(this.value);
+          // 判断 x 的值是普通值还是promise对象
+          // 如果是普通值，直接调用 resolve
+          // 如果是 promise 对象，查看 promise 对象的返回结果
+          // 再根据 promise 对象返回的结果，决定调用 resolve 还是调用 reject
+          resolvePromise(promise2, x, resolve, reject);
+        }, 0)
+      } else if(this.status === REJECTED) {
+        setTimeout(() => {
+          const x = onRejected(this.reason);
+          resolvePromise(promise2, x, resolve, reject);
+        }, 0);
+      } else {
+        // 订阅的过程
+        // 将成功和失败回调存储起来
+        this.onResolveCallback.push(() => {
+          setTimeout(() => {
+            const x = onFulFilled(this.value);
+            resolvePromise(promise2, x, resolve, reject);
+          }, 0);
+        });
+        this.onRejectCallback.push(() => {
+          setTimeout(() => {
+            const x = onRejected(this.reason);
+            resolvePromise(promise2, x, resolve, reject);
+          }, 0);
+        });
+      }
+    });
+    return promise2;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
 import Promise from "./Promise";
 
 const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    Math.random() >= 0.5
-      ? resolve('resolve')
-      : reject('reject');
-  }, 2000);
+  // setTimeout(() => {
+  //   Math.random() >= 0.5
+  //     ? resolve('resolve')
+  //     : reject('reject');
+  // }, 2000);
+  resolve('resolve');
 });
 
-promise.then((data) => {
-  console.log('success', data)
+function other() {
+  return new Promise((resolve, reject) => {
+    resolve('other');
+  })
+}
+
+const  p1= promise.then((data) => {
+  console.log('success', data);
+  return p1; // TODO: 返回自己会报 Chaining cycle detected for promise
+  // return 100;
 }, (error) => {
   console.log('error', error)
 })
 
-promise.then((data) => {
-  console.log('success2', data)
+p1.then((data) => {
+  console.log(data);
+  return other();
 }, (error) => {
-  console.log('error2', error)
+  console.log(error.message)
 })
+.then(data => console.log(data))
